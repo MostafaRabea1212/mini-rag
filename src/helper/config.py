6a146bb2153d0052f5472fc77ddea71313278321
Mostafa Rabea Shaban <mostafa6m666@gmail.com>
2026-08-31 07:26:32 +0300

    Add initial __init__.py and config.py files for controllers, helper, and models

---
+++ src/helper/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings , SettingsConfigDict
+
+class Settings(BaseSettings):
+
+    APP_NAME : str
+    APP_VERSION : str
+    OPENAI_API_KEY : str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+def get_settings():
+    return Settings()
