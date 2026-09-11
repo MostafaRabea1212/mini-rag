66e1752b3487945542a63704fe64aa62753039e8
Mostafa Rabea Shaban <mostafa6m666@gmail.com>
2026-09-11 15:21:37 +0300

    Fix typo in class name from BaseContoller to BaseController

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -3,7 +3,7 @@ import os
 import random
 import string
 
-class BaseContoller:
+class BaseController:
     def __init__(self):
         self.app_settings=get_settings()
         self.base_dir=os.path.dirname(os.path.dirname(__file__))
@@ -11,3 +11,4 @@ class BaseContoller:
 
     def generate_random_string(self, length: int=12):
         return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+    
