9bbe22381e5aa0bd06050f3077faaaf928f4f147
Mostafa Rabea Shaban <mostafa6m666@gmail.com>
2026-09-12 08:05:36 +0300

    Add Project and DataChunk models with validation logic

---
+++ src/models/db_schemes/data_chunk.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field ,validator
+from typing import Optional
+from bson import ObjectId
+
+class DataChunk(BaseModel):
+    _id : Optional[ObjectId]
+    chunk_text :str = Field(...,min_length=1)
+    chunk_metadata :dict 
+    chunk_order : int = Field(...,gt=0)
+    chunk_project_id : ObjectId
+
+    @validator('project_id')
+    def validate_project_id(cls , value):
+        if not value.isalnum():
+            raise ValueError('project_id must be alphanumeric')
+
+        return value
+
+    @validator('chunk_id')
+    def validate_chunk_id(cls , value):
+        if not value.isalnum():
+            raise ValueError('chunk_id must be alphanumeric')
+
+        return value
+
+    class Config:
+        arbitrary_types_allowed = True
