0f26e4e9b073f3fd07ad8e151705181b0e7eee5e
Mostafa Rabea Shaban <mostafa6m666@gmail.com>
2026-09-15 01:07:43 +0300

    Add ProjectModel class for managing project data and CRUD operations

---
+++ src/models/ProjectModel.py
@@ -0,0 +1,94 @@
+from .BaseDataModel import BaseDataModel
+from .db_schemes import Project
+from .enums import DataBaseEnum
+from bson.objectid import ObjectId
+
+""""
+Example of the database structure for the mini_rag application:
+Database: mini_rag
+│
+├── Collection: projects
+│      │
+│      └── Document (Project)
+│             {
+│               "_id": ObjectId("64f...a1"),
+│               "project_id": "1"
+│             }
+│
+└── Collection: chunks
+       │
+       └── من Project "1" — ده نفس الفولدر اللي لقيته عندك فعلاً
+              │      src/assets/files/1/vph7l1y3v44e_mostafa.txt
+              │
+              ├── Document (Chunk 1)
+              │      {
+              │        "_id": ObjectId("64f...c1"),
+              │        "chunk_text": "...",
+              │        "chunk_metadata": { },
+              │        "chunk_order": 1,
+              │        "chunk_project_id": ObjectId("64f...a1")
+              │      }
+              │
+              ├── Document (Chunk 2)
+              │      {
+              │        "_id": ObjectId("64f...c2"),
+              │        "chunk_text": "...",
+              │        "chunk_metadata": { },
+              │        "chunk_order": 2,
+              │        "chunk_project_id": ObjectId("64f...a1")
+              │      }
+              │
+              └── Document (Chunk 3)
+                     {
+                       "_id": ObjectId("64f...c3"),
+                       "chunk_text": "...",
+                       "chunk_metadata": { },
+                       "chunk_order": 3,
+                       "chunk_project_id": ObjectId("64f...a1")
+                     }
+"""
+class ProjectModel(BaseDataModel):
+
+    def __init__(self, db_client:object):
+        super().__init__(db_client=db_client)
+        self.collection=self.db_client[DataBaseEnum.COLLECTION_PROJECT_NAME.value]
+
+    async def create_project(self, project:Project):
+          
+        result=await self.collection.insert_one(project.dict(by_alias=True,exclude_unset=True))
+        project.id= result.inserted_id
+
+        return project
+
+    async def get_project_or_create_one(self, project_id : str):
+
+        record = await self.collection.find_one({
+            "project_id" : project_id
+        })
+
+        if record is None:
+            # create new project
+            project=Project(project_id=project_id)
+            project=await self.create_project(project=project)
+
+            return project
+        
+        return Project(**record)
+
+    async def get_all_projects(self,page:int=1,page_size:int=10):
+
+        # count total number od document
+
+        total_documents=await self.collection.count_documents({})
+
+        # calculate total number of pages 
+        total_pages=total_documents // page_size
+        if total_documents % page_size > 0 : 
+            total_pages += 1
+
+        curser=self.collection.find().skip( (page-1) * page_size ).limite(page_size)
+        projects=[]
+        async for document in curser:
+            projects.append(Project(**document))
+
+        return projects,total_pages
