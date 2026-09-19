8a19f5c86af218187a91b10a9aa4938420d6b61e
Mostafa Rabea Shaban <mostafa6m666@gmail.com>
2026-09-19 21:56:49 +0300

    Add VectorDB configuration and enhance BaseController for database path management

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -9,6 +9,17 @@ class BaseController:
         self.base_dir=os.path.dirname(os.path.dirname(__file__))
         self.files_dir=os.path.join(self.base_dir, "assets/files")
 
+        self.database_dir= os.path.join(self.base_dir,"asset/database")
+
     def generate_random_string(self, length: int=12):
         return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+
+    def get_data_base_path(self , db_name : str):
+        database_path =os.path.join(
+            self.database_dir,db_name
+            )
+        if not os.path.exists(database_path):
+            os.makedirs(database_path)
+
+        return database_path
     
