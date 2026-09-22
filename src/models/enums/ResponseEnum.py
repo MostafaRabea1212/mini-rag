7d00fd4fc7c91c9e9335c73580dc89b925669297
Mostafa Rabea Shaban <mostafa6m666@gmail.com>
2026-09-22 15:48:03 +0300

    Add NlpController import and extend ResponseSignal enum with new vector database statuses

--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -13,3 +13,7 @@ class ResponseSignal(Enum):
     PROJECT_NOT_FOUND_ERROR= "project_not_found"
     INSERT_INTO_VECTORDB_ERROR ="insert_into_vectordb_error"
     INSERT_INTO_VECTORDB_SUCCESS ="insert_into_vectordb_success"
+    VECTOR_COLLECTION_RETRIVED = "vectordb_collection_retrieved"
+    VECTORDB_SEARCH_ERROR = "vectordb_search_error"
+    VECTORDB_SEARCH_SUCCESS ="vectordb_search_success"
+    
